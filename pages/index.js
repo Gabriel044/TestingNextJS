a5c456855fc0ade22f8618556c678b4ff5dcf777
Gabriel044 <gabgar4@gmail.com>
2022-03-30 21:10:21 -0600

    Styling

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,15 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container">
-      Buen día profesor. Nombre: Santiago Gabriel Vallejo García Matrícula:
-      A01631816 Clase: Lab Web
+    <div className="container" style={{ textAlign: "center" }}>
+      <h1>Actividad 7.2</h1>
+      <i>Buen día profesor. </i>
+      <strong>Nombre:</strong> Santiago Gabriel Vallejo García
+      <br />
+      <strong>Matrícula:</strong>A01631816
+      <br />
+      <strong>Clase:</strong>Lab Web
+      <br />
     </div>
   );
 }
